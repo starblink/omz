f3643a01ca114cf2746200cf16a475b7ffcc675a
starblink <starblink@localhost.com>
2020-04-12 11:26:39 +0800

    modify .sh add chsh -s /usr/bin/zsh

--- inst_omz_and_configbymyself.sh
+++ inst_omz_and_configbymyself.sh
@@ -12,3 +12,5 @@ sed -i 's/fg=8/fg=10/' $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-a
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins
 
 mv ubuntu_zshrc $HOME/.zshrc
+
+chsh -s /usr/bin/zsh
