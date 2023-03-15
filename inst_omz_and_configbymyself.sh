3737261ee822a42b735d1556d00d913c32ee972e
starblink <starblink@localhost.com>
2023-03-15 14:35:09 +0800

    .

--- inst_omz_and_configbymyself.sh
+++ inst_omz_and_configbymyself.sh
@@ -3,20 +3,20 @@ apt update
 apt install -y zsh
 
 #安装oh my zsh
-rm -rf $HOME/.oh-my-zsh
-rm -f $HOME/.zshrc*
+rm -rf ~/.oh-my-zsh
+rm -f ~/.zshrc*
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #安装zsh-autosuggestions插件
-rm -rf $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-sed -i 's/fg=8/fg=10/' $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+rm -rf ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+sed -i 's/fg=8/fg=10/' ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 #安装zsh-autosuggestions插件
-rm -rf $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+rm -rf ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
-cp $HOME/omz/ubuntu_zshrc $HOME/.zshrc
+cp ~/omz/ubuntu_zshrc ~/.zshrc
 
-source $HOME/.zshrc
+source ~/.zshrc
