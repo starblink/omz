5e0a49594b7b291e585b95ce68c6fac34703a7ce
starblink <starblink@localhost.com>
2023-03-15 12:25:27 +0800

    .

--- inst_omz_and_configbymyself.sh
+++ inst_omz_and_configbymyself.sh
@@ -11,10 +11,12 @@ rm -f $HOME/.zshrc*
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #安装zsh-autosuggestions插件
+rm -rf $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone git://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 sed -i 's/fg=8/fg=10/' $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 #安装zsh-autosuggestions插件
+rm -rf $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 cp $HOME/omz/ubuntu_zshrc $HOME/.zshrc
