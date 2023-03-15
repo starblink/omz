37e292821f635551f6a77e5ee8b9ad5b2e3f0b11
starblink <starblink@localhost.com>
2023-03-15 14:11:48 +0800

    .

--- inst_omz_and_configbymyself.sh
+++ inst_omz_and_configbymyself.sh
@@ -10,6 +10,8 @@ rm -f $HOME/.zshrc*
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+exit
+
 #安装zsh-autosuggestions插件
 rm -rf $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
